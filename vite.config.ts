3e566c97aa3bea2e975ecbf8d2497e9fc597bb5c
--add <nikechukwu@gmail.com>
2024-05-17 00:19:50 -0400

    Added new code: Removes large number of dependencies and uses Vite to bundle and build library (uses Rollup under so configs can be ported)

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { fileURLToPath, URL } from "url";
+import dts from 'vite-plugin-dts'
+
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./lib", import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      name: 'Flutterwave',
+      fileName: 'flw-ts'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+});
